a38c10e176dc0109736114abdee246a8af2da8ac
ChungTran <chungtv@tadi.io>
2017-10-16 15:26:20 +0700

    Initial Commit

---
+++ AdMod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdMod
+//
+//  Created by ChungTran on 10/16/17.
+//  Copyright © 2017 ChungTran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
