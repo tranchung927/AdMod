4da53626c56181212445e3da85fa80dd54f45ce8
ChungTran <chungtv@tadi.io>
2017-10-17 11:40:49 +0700

    Admod c

--- AdMod/ViewController.swift
+++ AdMod/ViewController.swift
@@ -4,22 +4,17 @@
 //
 //  Created by ChungTran on 10/16/17.
 //  Copyright © 2017 ChungTran. All rights reserved.
-//
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        GoogleAdMob.sharedInstance.initializeBannerView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showInterestinal(_ sender: UIButton) {
+        GoogleAdMob.sharedInstance.showInterstitial()
     }
-
-
 }
 
---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, ’10.3’
+  use_frameworks!
+target 'AdMod' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+	pod 'Google-Mobile-Ads-SDK'
+end
